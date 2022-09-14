ad4ce9f421b828d95342dc727b26478858eaef16
omorka <36473263+omorka@users.noreply.github.com>
2022-09-14 05:04:33 -0400

    Initial commit

---
+++ Data/DataUtility.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+
+namespace AddressBook.Data
+{
+    public static class DataUtility
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+
+        public static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+            };
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using AddressBook.Data;
+using AddressBook.Services;
+using AddressBook.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseNpgsql(DataUtility.GetConnectionString(builder.Configuration)));
+
+builder.Services.AddScoped<IImageService, BasicImageService>();
+
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+var dbContext = app.Services.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+await dbContext.Database.MigrateAsync();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
